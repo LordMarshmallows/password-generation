3adcf39b9c98812bbfd8b1d1f3c3b3f25c54df60
LordMarshmallows <46316876+LordMarshmallows@users.noreply.github.com>
2019-02-17 14:42:53 +0300

    Add files via upload

---
+++ WpfApp20/Pas.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp20
+{
+  public class Pas
+    {
+        public string Pa { get; set; }
+        public string Sec { get; set; }
+    }
+}
